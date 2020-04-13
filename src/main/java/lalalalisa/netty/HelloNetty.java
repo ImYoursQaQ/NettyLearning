69e2dc137789fb80ac31195d8e01673f29cfcbe2
Kimi John <1948976637@qq.com>
2020-04-13 23:44:10 +0800

    优化

--- src/main/java/lalalalisa/netty/HelloNetty.java
+++ src/main/java/lalalalisa/netty/HelloNetty.java
@@ -1,9 +1,8 @@
 package lalalalisa.netty;
 
-import com.mashibing.io.aio.Server;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
